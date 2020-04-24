1dc579c675763563854c2be59a09af4f9203fffc
Gabrielli Correa <gabriellicorrea7@gmail.com>
2020-04-24 12:17:20 -0300

    adicionando o PersistenceContext

--- src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/api/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.algaworks.osworks.api.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,26 +14,17 @@ import com.algaworks.osworks.domain.model.Cliente;
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-			cliente1.setId(1L);
-			cliente1.setNome("Gabi");
-			cliente1.setEmail("gabi@gabi.com");
-			cliente1.setTelefone("12 3456-7890");
-			
-		var cliente2 = new Cliente();
-			cliente2.setId(1L);
-			cliente2.setNome("Gabi2");
-			cliente2.setEmail("gabi2@gabi.com");
-			cliente2.setTelefone("12 3456-7890");
-			
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class)
+				.getResultList();
+		
 	}
 
 }
 
-//Uso o List pq é uma lista de Clientes
-//List<Cliente> é da classe Cliente model
-//crio as variaveis de clientes
-//e retorno o Arrays.asList com os cliente.
+//criou a consulta com o "createQuery"
+//jpql para pegar a listae tipo 
