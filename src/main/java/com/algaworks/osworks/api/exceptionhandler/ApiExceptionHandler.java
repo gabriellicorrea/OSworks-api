c917387e0c2b08307df52d5850fca9ebda32bb63
Gabrielli Correa <gabriellicorrea7@gmail.com>
2020-04-27 16:13:20 -0300

    Tratando excessoes

--- src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -13,14 +13,29 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.algaworks.osworks.exception.NegocioException;
 
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	
 	@Autowired
 	private MessageSource messageSource;
+	
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<Object> handleNegocio(NegocioException ex, WebRequest request) {
+		var status = HttpStatus.BAD_REQUEST;
+		var problema = new Problema();
+		problema.setStatus(status.value());
+		problema.setTitulo(ex.getMessage());
+		problema.setDatahora(LocalDateTime.now());
+		
+		return handleExceptionInternal(ex, problema, new HttpHeaders(), status, request);
+	}
+	
 	
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
@@ -39,7 +54,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		var problema = new Problema();
 		problema.setStatus(status.value());
-		problema.setTitulo("Um ou mais campos estão invalidos. faca o preenchimento correto e tnte novamente.");
+		problema.setTitulo("Um ou mais campos estão invalidos. faca o preenchimento correto e tente novamente.");
 		problema.setDatahora(LocalDateTime.now());
 		problema.setCampos(campos);
 		
@@ -47,3 +62,12 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 }
+
+		
+		
+		
+	
+
+	
+	
+
--- src/main/java/com/algaworks/osworks/domain/service/CadastroClienteService.java
+++ src/main/java/com/algaworks/osworks/domain/service/CadastroClienteService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
+import com.algaworks.osworks.exception.NegocioException;
 
 @Service
 public class CadastroClienteService {
@@ -13,6 +14,12 @@ public class CadastroClienteService {
 	private  ClienteRepository clienteRepository;
 	
 	public Cliente salvar(Cliente cliente) {
+		Cliente clienteExistente = clienteRepository.findByEmail(cliente.getEmail());
+		
+		if (clienteExistente != null && !clienteExistente.equals(cliente)) {
+			throw new NegocioException("Já existe um cliente cadastrado com esse email.");
+		}
+				
 		return clienteRepository.save(cliente);
 	}
 		
